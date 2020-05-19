9d99a57733fe32839a5caa7b3419c9fa6f2e747c
André Simmert <intellent@users.noreply.github.com>
2020-05-19 15:09:53 +0200

    [improve] EventBaseRepository::findById() expands event locations and lecturers.

--- src/Client/Repository/EventBaseRepository.php
+++ src/Client/Repository/EventBaseRepository.php
@@ -7,6 +7,19 @@ use Priorist\AIS\Client\Collection;
 class EventBaseRepository extends AbstractSearchableRepository
 {
     /**
+     * Returns a single event base having child relations expanded.
+     *
+     * @param int $id The unique ID of the event base
+     *
+     * @return array The event base as array or NULL, if matching event base was not found
+     */
+    public function findById(int $id, array $params = []) : ?array
+    {
+        return $this->fetchSingle($id, ['expand' => '~all,events.location,events.lecturers'], $params);
+    }
+
+
+    /**
      * Returns a collection of all event bases including their event records
      *
      * @param array $params Optional query parameters to filter the results
