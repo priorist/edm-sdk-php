9115cdc7bf2f74e7a0e852b99976ef7ba64a6195
André Simmert <intellent@users.noreply.github.com>
2020-06-09 15:00:24 +0200

    [docs] Updated PHPdocs.

--- src/Client/Repository/EventBaseRepository.php
+++ src/Client/Repository/EventBaseRepository.php
@@ -9,13 +9,26 @@ class EventBaseRepository extends AbstractSearchableRepository
     /**
      * Returns a single event base having child relations expanded.
      *
+     * @param string $slug The unique slug of the event base
+     *
+     * @return array The event base as array or NULL, if matching event base was not found
+     */
+    public function findBySlug(string $slug, array $params = []) : ?array
+    {
+        return $this->findById($slug, $params);
+    }
+
+
+    /**
+     * Returns a single event base having child relations expanded.
+     *
      * @param int $id The unique ID of the event base
      *
      * @return array The event base as array or NULL, if matching event base was not found
      */
-    public function findById(int $id, array $params = []) : ?array
+    public function findById($id, array $params = []) : ?array
     {
-        return $this->fetchSingle($id, ['expand' => '~all,events.location,events.lecturers'], $params);
+        return parent::fetchSingle($id, ['expand' => '~all,events.location,events.lecturers'], $params);
     }
 
 
