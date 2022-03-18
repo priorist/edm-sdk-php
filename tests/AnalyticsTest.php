4ce837edb118bc7c5d72b11402f761f9768406a7
Fabian <fabian@priorist.com>
2022-03-18 14:48:45 +0100

    Refactor trackTest + add test for trackWithDefault

--- tests/AnalyticsTest.php
+++ tests/AnalyticsTest.php
@@ -5,29 +5,41 @@ use PHPUnit\Framework\TestCase;
 use Priorist\EDM\Client\Client;
 use Priorist\EDM\Helper\AnalyticsHelper;
 
-
 class AnalyticsTest extends TestCase
 {
     public function testTrack()
+    {
+        $client = new Client(getenv('EDM_URL'), getenv('CLIENT_ID'), getenv('CLIENT_SECRET'));
+        $analytics = $client->analytics;
+
+        $trackingEvent = "event_viewed";
+        $event = 1;
+        $eventBase = 1;
+        $contexts = [1, 2];
+
+        $analytics->setUserId(AnalyticsHelper::getHashedUserId());
+        $analytics->setUrl(AnalyticsHelper::getFullUrl());
+        $analytics->setReferrer(AnalyticsHelper::getReferrerUrl());
+        $analytics->setUtmMedium(AnalyticsHelper::getUtmParameter('medium'));
+        $analytics->setUtmSource(AnalyticsHelper::getUtmParameter('source'));
+        $analytics->setUtmCampaign(AnalyticsHelper::getUtmParameter('campaign'));
+
+        $response = $client->analytics->track($trackingEvent, $event, $eventBase, $contexts);
+
+        $this->assertIsArray($response);
+    }
+
+    public function testTrackWithDefaults()
     {
         $client = new Client(getenv('EDM_URL'), getenv('CLIENT_ID'), getenv('CLIENT_SECRET'));
 
         $trackingEvent = "event_viewed";
         $event = 1;
         $eventBase = 1;
-        $url = AnalyticsHelper::getFullUrl();
-        $referrer = AnalyticsHelper::getReferrerUrl();
-        $userId = AnalyticsHelper::getHashedUserId();
         $contexts = [1, 2];
-        $utmParameters = AnalyticsHelper::getUtmParameters();
-        $utmMedium = $utmParameters["utm_medium"];
-        $utmSource = $utmParameters["utm_source"];
-        $utmCampaign = $utmParameters["utm_campaign"];
 
-        $response = $client->analytics->track($trackingEvent, $event, $eventBase, $url, $referrer, $userId, $contexts, $utmMedium, $utmSource, $utmCampaign);
+        $response = $client->analytics->trackWithDefaults($trackingEvent, $event, $eventBase, $contexts);
 
         $this->assertIsArray($response);
-
-        return $response;
     }
 }
