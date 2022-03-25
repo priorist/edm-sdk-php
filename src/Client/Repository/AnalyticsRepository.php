6863db9a7dca5e92140afbc70338efb36a18ac55
Fabian <fabian@priorist.com>
2022-03-25 16:40:59 +0100

    Don't set UTM parameters and remove context

--- src/Client/Repository/AnalyticsRepository.php
+++ src/Client/Repository/AnalyticsRepository.php
@@ -14,29 +14,27 @@ class AnalyticsRepository extends AbstractRepository
      * @param string    $trackingEvent  The type of event that should be tracked.
      * @param int       $eventBase      The ID of the EDM event base.
      * @param int       $event          Optional. The ID of the EDM event.
-     * @param int       $context        Optional. The EDM context the website is running in.
      * @param string    $userId         Optional. Unique identifier of a tracked user. Defaults to a hashed user id based on ip address and user agent.
      * @param string    $url            Optional. The current URL being tracked.
      * @param string    $referrer       Optional. The current URL referrer.
-     * @param string    $utmMedium      Optional. The value of 'utm_medium' query parameter.
-     * @param string    $utmSource      Optional. The value of 'utm_source' query parameter.
-     * @param string    $utmCampaign    Optional. The value of 'utm_campaign' query parameter.
+     * @param string    $utmMedium      Optional. The value to be used as 'utm_medium' query parameter. If not provided, EDM will try to parse it from URL.
+     * @param string    $utmSource      Optional. The value to be used as 'utm_source' query parameter. If not provided, EDM will try to parse it from URL.
+     * @param string    $utmCampaign    Optional. The value to be used as 'utm_campaign' query parameter. If not provided, EDM will try to parse it from URL.
      *
      * @return array|null The decoded response as array or NULL, if the JSON was malformed.
      */
-    public function track(string $trackingEvent, int $eventBase, ?int $event = null, ?int $context = null, ?string $userId = null, ?string $url = null, ?string $referrer = null, ?string $utmMedium = null, ?string $utmSource = null, ?string $utmCampaign = null): ?array
+    public function track(string $trackingEvent, int $eventBase, ?int $event = null, ?string $userId = null, ?string $url = null, ?string $referrer = null, ?string $utmMedium = null, ?string $utmSource = null, ?string $utmCampaign = null): ?array
     {
         $data = [
             "name" => $trackingEvent,
             "event_base" => $eventBase,
             "event" => $event,
-            "context" => $context,
             "userId" => $this->userId ?? $userId ?? AnalyticsHelper::getHashedUserId(),
             "url" => $url ?? AnalyticsHelper::getFullUrl(),
             "referrer" => $referrer ?? AnalyticsHelper::getReferrerUrl(),
-            "utm_medium" => $utmMedium ?? AnalyticsHelper::getUtmParameter('medium'),
-            "utm_source" => $utmSource ?? AnalyticsHelper::getUtmParameter('source'),
-            "utm_campaign" => $utmCampaign ?? AnalyticsHelper::getUtmParameter('campaign')
+            "utm_medium" => $utmMedium,
+            "utm_source" => $utmSource,
+            "utm_campaign" => $utmCampaign
         ];
 
         return $this->create($data);
@@ -48,7 +46,6 @@ class AnalyticsRepository extends AbstractRepository
      * @param string    $trackingEvent  The type of event that should be tracked.
      * @param int       $eventBase      The ID of the EDM event base.
      * @param int       $event          Optional. The ID of the EDM event.
-     * @param int       $context        Optional. The EDM context the website is running in.
      * @param string    $userId         Optional. Unique identifier of a tracked user. Defaults to a hashed user id based on ip address and user agent.
      * @param string    $url            Optional. The current URL being tracked.
      * @param string    $referrer       Optional. The current URL referrer.
@@ -58,10 +55,10 @@ class AnalyticsRepository extends AbstractRepository
      *
      * @return array|null The decoded response as array or NULL, if the JSON was malformed.
      */
-    public function trackSilently(string $trackingEvent, int $eventBase, ?int $event = null, ?int $context = null, ?string $userId = null, ?string $url = null, ?string $referrer = null, ?string $utmMedium = null, ?string $utmSource = null, ?string $utmCampaign = null): ?array
+    public function trackSilently(string $trackingEvent, int $eventBase, ?int $event = null, ?string $userId = null, ?string $url = null, ?string $referrer = null, ?string $utmMedium = null, ?string $utmSource = null, ?string $utmCampaign = null): ?array
     {
         try {
-            return $this->track($trackingEvent, $eventBase, $event, $context, $userId, $url, $referrer, $utmMedium, $utmSource, $utmCampaign);
+            return $this->track($trackingEvent, $eventBase, $event, $userId, $url, $referrer, $utmMedium, $utmSource, $utmCampaign);
         } catch (\Throwable $e) {
             return $e->getMessage();
         }
